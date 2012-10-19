5a02c789ae86454c45e89b4f84cb7b40e98d0d4a
Daniel <daniel.rekshan@gmail.com>
2012-10-18 21:56:41 -0700

    Update lib/postmark/message_extensions/mail.rb

--- lib/postmark/message_extensions/mail.rb
+++ lib/postmark/message_extensions/mail.rb
@@ -4,7 +4,7 @@ module Mail
     include Postmark::SharedMessageExtensions
     
     def html?
-      content_type && content_type.include?('text/html')
+      false
     end
     
     def body_html
